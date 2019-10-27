fce137d6b04629a0dc5747d20e4fa465f6dd83d8
Pumpkin-Jam <iharu-e60@docomo.ne.jp>
2019-10-27 14:15:34 +0900

    追加しました

---
+++ PumpkinSeer.py
@@ -0,0 +1,33 @@
+class PumpkinSeer(object):
+    def __init__(self):
+        pass
+    
+    def initialize(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def dayStart(self):
+        pass
+    
+    def talk(self):
+        pass
+    
+    def whisper(self):
+        pass
+
+    def vote(self):
+        pass
+
+    def attack(self):
+        pass
+    
+    def divine(self):
+        pass
+    
+    def guard(self):
+        pass
+    
+    def finish(self):
+        pass
